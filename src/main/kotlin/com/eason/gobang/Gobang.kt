9698199ef9071d8555c30293d81e77e6d4409b68
Jinghua Li <jinhli@thoughtworks.com>
2021-06-08 17:04:57 +0800

    Determine if it is five points in row or column

--- src/main/kotlin/com/eason/gobang/Gobang.kt
+++ src/main/kotlin/com/eason/gobang/Gobang.kt
@@ -2,6 +2,8 @@ package com.eason.gobang
 
 class Gobang(private val row: Int = 10, private val column: Int = 10) {
     private val points = mutableSetOf<Point>()
+    var currentChessPiece: ChessPiece? = null
+    var currentPoint: Point? = null
 
     init {
         for (rowIndex in 0 until row) {
@@ -23,15 +25,64 @@ $rowsContent""".trimIndent()
     }
 
     fun setChessPieces(rowIndex: Int, columnIndex: Int) {
-        val chessPiece : ChessPiece = getNeedInputChessPiece()
-        val point  = points.first { point -> point.rowIndex == rowIndex && point.columnIndex == columnIndex }
-        point.chessPiece = chessPiece
+        currentChessPiece = getNeedInputChessPiece()
+        currentPoint = points.first { point -> point.rowIndex == rowIndex && point.columnIndex == columnIndex }
+        currentPoint!!.chessPiece = currentChessPiece
     }
 
     fun getNeedInputChessPiece(): ChessPiece {
         val chessPieces = points.filter { point -> point.chessPiece != null }.map { point: Point -> point.chessPiece }
-        val blackPiecesCount = chessPieces.filter{piece -> piece == ChessPiece.BlACK}.count()
+        val blackPiecesCount = chessPieces.filter { piece -> piece == ChessPiece.BlACK }.count()
         val whitePiecesCount = chessPieces.filter { piece -> piece == ChessPiece.WHITE }.count()
         return if (blackPiecesCount == whitePiecesCount) ChessPiece.BlACK else ChessPiece.WHITE
+    }
+
+    fun isWin(): Boolean {
+        var allPointWithSameInputChessPiece =
+            points.filter { point -> point.chessPiece != null && point.chessPiece == currentChessPiece }
+        if (allPointWithSameInputChessPiece.count() < 5) {
+            return false
+        }
+        val allAcrossPointsColumnIndexes =
+            allPointWithSameInputChessPiece.filter { point -> point.rowIndex == currentPoint!!.rowIndex }
+                .map { it.columnIndex }.sorted()
+
+        if (isHaveSerialFivePoints(allAcrossPointsColumnIndexes)) {
+            return true
+        }
+
+        val allVerticalPointsColumnIndexes =
+            allPointWithSameInputChessPiece.filter { point -> point.columnIndex == currentPoint!!.columnIndex }
+                .map { it.rowIndex }.sorted()
+
+        if (isHaveSerialFivePoints(allVerticalPointsColumnIndexes)) {
+            return true
+        }
+
+        return false
+    }
+
+    private fun isHaveSerialFivePoints(indexes: List<Int>): Boolean {
+        if (indexes.count() < 5) {
+            return false
+        }
+
+        val allIndexes = indexes.minOrNull()!!.rangeTo(indexes.maxOrNull()!!)
+        val leftPoints = allIndexes.subtract(indexes)
+
+        if (allIndexes.count() == 5 && leftPoints.count() == 0) {
+            return true
+        }
+
+        leftPoints.forEachIndexed { index, value ->
+            if (index + 1 == leftPoints.count()) {
+                return false
+            }
+
+            if (leftPoints.elementAt(index + 1) - value >= 5) {
+                return true
+            }
+        }
+        return false
     }
 }
--- src/test/kotlin/com/eason/gobang/GobangTest.kt
+++ src/test/kotlin/com/eason/gobang/GobangTest.kt
@@ -29,8 +29,8 @@ internal class GobangTest {
 
     @Test
     fun should_return_chessboard_when_app_start() {
-        val chessboard:String = gobang.getChessBoard()
-        val expectChessboard:String  = """
+        val chessboard: String = gobang.getChessBoard()
+        val expectChessboard: String = """
         0 1 2 3 4 5 6 7 8 9
       0 ┌─┬─┬─┬─┬─┬─┬─┬─┬─┐
       1 ├─┼─┼─┼─┼─┼─┼─┼─┼─┤
@@ -49,8 +49,8 @@ internal class GobangTest {
     @Test
     fun should_return_chessboard_with_black_one_chess_pieces() {
         gobang.setChessPieces(4, 5)
-        val chessboard:String = gobang.getChessBoard()
-        val expectChessboard:String  = """
+        val chessboard: String = gobang.getChessBoard()
+        val expectChessboard: String = """
         0 1 2 3 4 5 6 7 8 9
       0 ┌─┬─┬─┬─┬─┬─┬─┬─┬─┐
       1 ├─┼─┼─┼─┼─┼─┼─┼─┼─┤
@@ -70,8 +70,8 @@ internal class GobangTest {
     fun should_return_chessboard_with_white_one_chess_pieces() {
         gobang.setChessPieces(4, 5)
         gobang.setChessPieces(5, 6)
-        val chessboard:String = gobang.getChessBoard()
-        val expectChessboard:String  = """
+        val chessboard: String = gobang.getChessBoard()
+        val expectChessboard: String = """
         0 1 2 3 4 5 6 7 8 9
       0 ┌─┬─┬─┬─┬─┬─┬─┬─┬─┐
       1 ├─┼─┼─┼─┼─┼─┼─┼─┼─┤
@@ -85,5 +85,86 @@ internal class GobangTest {
       9 └─┴─┴─┴─┴─┴─┴─┴─┴─┘
         """.trimIndent()
         Assertions.assertEquals(expectChessboard, chessboard)
+    }
+
+    @Test
+    fun should_return_no_win_when_app_init() {
+        val isWin = gobang.isWin()
+        Assertions.assertFalse(isWin)
+    }
+
+    @Test
+    fun should_return_win_when_black_chess_piece_equals_5() {
+        gobang.setChessPieces(2, 1)
+        gobang.setChessPieces(1, 1)
+        gobang.setChessPieces(3, 2)
+        gobang.setChessPieces(2, 2)
+        gobang.setChessPieces(4, 3)
+        gobang.setChessPieces(3, 3)
+        gobang.setChessPieces(5, 4)
+        gobang.setChessPieces(4, 4)
+        gobang.setChessPieces(6, 5)
+        val isWin = gobang.isWin()
+        Assertions.assertTrue(isWin)
+    }
+
+    @Test
+    fun should_return_win_when_black_chess_piece_across_equals_5() {
+        gobang.setChessPieces(2, 1)
+        gobang.setChessPieces(1, 1)
+        gobang.setChessPieces(2, 2)
+        gobang.setChessPieces(1, 2)
+        gobang.setChessPieces(2, 3)
+        gobang.setChessPieces(1, 3)
+        gobang.setChessPieces(2, 4)
+        gobang.setChessPieces(1, 4)
+        gobang.setChessPieces(2, 5)
+        val isWin = gobang.isWin()
+        Assertions.assertTrue(isWin)
+    }
+
+    @Test
+    fun should_return_no_win_when_black_chess_piece_across_equals_5() {
+        gobang.setChessPieces(2, 1)
+        gobang.setChessPieces(1, 1)
+        gobang.setChessPieces(2, 2)
+        gobang.setChessPieces(1, 2)
+        gobang.setChessPieces(2, 3)
+        gobang.setChessPieces(1, 3)
+        gobang.setChessPieces(2, 6)
+        gobang.setChessPieces(1, 4)
+        gobang.setChessPieces(2, 5)
+        val isWin = gobang.isWin()
+        Assertions.assertFalse(isWin)
+    }
+
+    @Test
+    fun should_return_win_when_black_chess_piece_vertical_equals_5() {
+        gobang.setChessPieces(1, 2)
+        gobang.setChessPieces(1, 1)
+        gobang.setChessPieces(2, 2)
+        gobang.setChessPieces(2, 1)
+        gobang.setChessPieces(3, 2)
+        gobang.setChessPieces(3, 1)
+        gobang.setChessPieces(4, 2)
+        gobang.setChessPieces(4, 1)
+        gobang.setChessPieces(5, 2)
+        val isWin = gobang.isWin()
+        Assertions.assertTrue(isWin)
+    }
+
+    @Test
+    fun should_return_no_win_when_black_chess_piece_vertical_equals_5() {
+        gobang.setChessPieces(1, 2)
+        gobang.setChessPieces(1, 1)
+        gobang.setChessPieces(2, 2)
+        gobang.setChessPieces(2, 1)
+        gobang.setChessPieces(3, 2)
+        gobang.setChessPieces(3, 1)
+        gobang.setChessPieces(4, 2)
+        gobang.setChessPieces(4, 1)
+        gobang.setChessPieces(7, 2)
+        val isWin = gobang.isWin()
+        Assertions.assertFalse(isWin)
     }
 }

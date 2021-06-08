79e95797683be100d02ae2c70fa157701c810b16
Jinghua Li <jinhli@thoughtworks.com>
2021-06-08 18:06:51 +0800

    Replace if else with when express

--- src/main/kotlin/com/eason/gobang/Point.kt
+++ src/main/kotlin/com/eason/gobang/Point.kt
@@ -8,37 +8,16 @@ class Point(val rowIndex: Int, val columnIndex: Int) {
             return "${chessPiece!!.value}─"
         }
 
-        if (rowIndex == 0 && columnIndex in 1 until 9) {
-            return "┬─"
+        return when {
+            rowIndex == 0 && columnIndex in 1 until 9 -> "┬─"
+            rowIndex == 9 && columnIndex in 1 until 9 -> "┴─"
+            columnIndex == 0 && rowIndex in 1 until 9 -> "├─"
+            columnIndex == 9 && rowIndex in 1 until 9 -> "┤"
+            rowIndex == 0 && columnIndex == 0 -> "┌─"
+            rowIndex == 9 && columnIndex == 0 -> "└─"
+            rowIndex == 0 && columnIndex == 9 -> "┐"
+            rowIndex == 9 && columnIndex == 9 -> "┘"
+            else -> "┼─"
         }
-
-        if (rowIndex == 9 && columnIndex in 1 until 9) {
-            return "┴─"
-        }
-
-        if (columnIndex == 0 && rowIndex in 1 until 9) {
-            return "├─"
-        }
-
-        if (columnIndex == 9 && rowIndex in 1 until 9) {
-            return "┤"
-        }
-
-        if (rowIndex == 0 && columnIndex == 0) {
-            return "┌─"
-        }
-
-        if (rowIndex == 9 && columnIndex == 0) {
-            return "└─"
-        }
-
-        if (rowIndex == 0 && columnIndex == 9) {
-            return "┐"
-        }
-
-        if (rowIndex == 9 && columnIndex == 9) {
-            return "┘"
-        }
-        return "┼─"
     }
 }

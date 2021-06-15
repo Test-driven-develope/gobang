49b62ed4afae6642c4f45ce8c6a406ec64f51523
Jinghua Li <jinhli@thoughtworks.com>
2021-06-15 18:46:11 +0800

    Pint the init gobang and greetings when app start

--- src/test/kotlin/com/eason/gobang/AppKtTest.kt
+++ src/test/kotlin/com/eason/gobang/AppKtTest.kt
@@ -19,7 +19,19 @@ internal class AppKtTest {
 
     @Test
     fun should_print_message_when_game_start() {
-        val expect = "欢迎来到五子连珠小游戏，分为黑子(◉)和白子(◯):"
+        val expect = """欢迎来到五子连珠小游戏，分为黑子(◉)和白子(◯):
+  0 1 2 3 4 5 6 7 8 9
+0 ┌─┬─┬─┬─┬─┬─┬─┬─┬─┐
+1 ├─┼─┼─┼─┼─┼─┼─┼─┼─┤
+2 ├─┼─┼─┼─┼─┼─┼─┼─┼─┤
+3 ├─┼─┼─┼─┼─┼─┼─┼─┼─┤
+4 ├─┼─┼─┼─┼─┼─┼─┼─┼─┤
+5 ├─┼─┼─┼─┼─┼─┼─┼─┼─┤
+6 ├─┼─┼─┼─┼─┼─┼─┼─┼─┤
+7 ├─┼─┼─┼─┼─┼─┼─┼─┼─┤
+8 ├─┼─┼─┼─┼─┼─┼─┼─┼─┤
+9 └─┴─┴─┴─┴─┴─┴─┴─┴─┘
+请黑子(◉)输入行列坐标(如3,4):""".trimIndent()
         main()
         Assertions.assertEquals(expect, outputStreamCaptor.toString().trim())
     }
